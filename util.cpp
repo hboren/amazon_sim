92a120013e4b0fbfd692666456e136e26ad8e410
Haley Boren <hboren@usc.edu>
2022-09-26 21:51:18 +0000

    Committing additional files checkpoint

--- util.cpp
+++ util.cpp
@@ -15,16 +15,36 @@ std::string convToLower(std::string src)
     to a set of words based on the criteria given in the assignment **/
 std::set<std::string> parseStringToWords(string rawWords)
 {
+	//make the word lowercase
+	rawWords = convToLower(rawWords);
+	set<string> keywords;
+	int curr = 0;
+	int start = 0;
+	//while we haven't reached the end of the string
+	while(rawWords[curr]){
+		//if a punctuation or a space is found, make a substring from the start
+		//to right before the punctuation, trim spaces, update curr appropriately
+		if(ispunct(rawWords[curr]) || isspace(rawWords[curr])){
+			string str1 = rawWords.substr(start, curr-start);
+			trim(str1);
+			//if substring is at least 2 characters, insert into set and 
+			//update start to be right after found punctuation
+			if(str1.length() > 1){
+				keywords.insert(str1);
+			}
+			start = curr + 1;
+		}
+		curr++;
+	}
+	//create a substring of the remaining string, trim, and if at least 2 chars,
+	//insert into keyword set
+	string str2 = rawWords.substr(start, rawWords.length()-start);
+	trim(str2);
+	if(str2.length() > 1){
+		keywords.insert(str2);
+	}
 
-
-
-
-
-
-
-
-
-
+	return keywords;
 }
 
 /**************************************************
--- util.h
+++ util.h
@@ -13,19 +13,43 @@
 template <typename T>
 std::set<T> setIntersection(std::set<T>& s1, std::set<T>& s2)
 {
+	//return a set with only things that appear in both sets
+	std::set<T> intersection_;
 
+	//add items that are in both set 1 and 2 into to intersection set
+	for(typename std::set<T>::iterator it1=s1.begin(); it1 != s1.end(); ++it1)
+	{
+		//check if item in s1 is also in set2. if so, add to set
+		if(s2.find(*it1) != s2.end()){
+			intersection_.insert(*it1);
+		}
+	}
 
-
-
+	return intersection_;
 
 }
 template <typename T>
 std::set<T> setUnion(std::set<T>& s1, std::set<T>& s2)
 {
+	//return a set with things that appear either set
+	std::set<T> union_;
 
+	//add items from set1 to union
+	for(typename std::set<T>::iterator it1=s1.begin(); it1 != s1.end(); ++it1)
+	{
+		union_.insert(*it1);
+	}
 
+	//add items from set2 that aren't already in set1 to union
+	for(typename std::set<T>::iterator it2=s2.begin(); it2 != s2.end(); ++it2)
+	{
+		//check if item in set2 is in set1 (aka already added). if not, add to union
+  	if(s1.find(*it2) == s1.end()){
+			union_.insert(*it2);
+		}
+	}
 
-
+	return union_;
 
 }
 
